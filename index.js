64e62b15659f29ba1c34bf48d0e37572afae48ec
webdeveloper272006 <web213783@gmail.com>
2024-12-18 14:41:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let eligibility = 18
+let age = eligibility>=18 ?'can drive':'cannot drive'
+console.log(age)
+let number = -10
+let limits = number<0 ?'Number is NEGATIVE':'Number is POSITIVE'
+console.log(limits)
+
+let word = "danim"
+console.log('typeof word is', typeof(word))
+
+let numbers = 190
+console.log('Type of numbers is',typeof(numbers))
+
+let scentence;
+console.log('Type of describe is',typeof(scentence))
+
+cost1 = 67
+cost2 = 647
+cost3 = 228
+cost4 = 830
+let addition = cost1+cost2+cost3+cost4
+console.log('The Total Cost of Items is',addition)
+
+let length = 270
+let breadth = 180
+let perimeter = 2*length + 2*breadth
+console.log("The Perimeter of Rectangle is",perimeter)
+
+ str = 'Honesty is the key of Success'
+console.log("the substrings are",str.substr("Honesty"))
+
+let paragraph = "this is a test. This is only a test"
+console.log('The Last Occurrance of "is" is',paragraph.lastIndexOf("is"))
+

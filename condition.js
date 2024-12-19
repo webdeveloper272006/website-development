1403fc6304b9ef5167661c23dc22f0bb6b14fd5e
webdeveloper272006 <web213783@gmail.com>
2024-12-19 14:50:14 +0500

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,27 @@
+// question1
+let greeting = "hello+john"
+let age = 5
+let ageinmonths = (age)*12
+console.log(age)
+console.log("The age in months is ",ageinmonths)
+
+// Question2
+let num =10
+
+if(num*2){
+    console.log("even")
+}
+else{
+    console.log("odd")
+}
+
+// Question3
+num=0
+if(num>0){
+    message="Positive Number"
+}
+else if(num<0){
+    message="Negative Number"
+}
+else 
+{console.log("ZERO")}

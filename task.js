d3d04fb7df81249d12d748b50d1213a0652bc5d8
webdeveloper272006 <web213783@gmail.com>
2024-12-24 14:54:07 +0500

    Add files via upload

--- task.js
+++ task.js
@@ -26,7 +26,36 @@
 // alert(`the converted value of celcius in fahrenhiet is ${fahrenheit}`)
 
 // Second Method
-function convert_fahrenheit(celcius){
-    return celcius*(9/5) +32
+// function convert_fahrenheit(celcius){
+//     return celcius*(9/5) +32
+// }
+// alert(convert_fahrenheit(prompt("Enter any temperature")))
+
+
+// // Question 3
+
+// age = prompt("enter your age to judge")
+// function check_age(age){
+//     if(age<18){
+//     return "Minor"}
+//     else if(age>=18 && age<60){
+//         return "adult"
+//     }
+//     else{
+//         return "Senior citizens"
+//     }
+// }
+// alert(check_age(age))
+
+// Question 4
+
+function is_even_odd(num){
+    if(num%2== 0){
+        return "Number is even"
+    }
+    else{
+        return "Number is odd"
+    }
 }
-alert(convert_fahrenheit(prompt("Enter any temperature")))
+Numbers = prompt("Enter your Number to see if it is even or odd")
+alert(is_even_odd (number))

65d3cb31ca9a5ffe1be180aad1bbcd34425b076c
webdeveloper272006 <web213783@gmail.com>
2024-12-26 14:14:27 +0500

    Add files via upload

---
+++ object.js
@@ -0,0 +1,31 @@
+// Question:1
+
+let person={
+    Name : "john Sen",
+    Age : 24,
+    isStudent : true,
+}
+person.Age = 23
+person.Hobby = "working"
+console.log(person);
+
+// Question:2
+
+const car={
+    Brand : "Hundai",
+    Model : "Latest",
+    Year : 2024,
+    Owner:{
+     Name :"William",
+     Age : 25,
+     Address:{
+        City : "UK",
+        Zip : 77890,
+     }
+    }
+}
+car.Owner.Age= 23
+car.Owner.Phone =2672646
+console.log(car.Model)
+console.log(car.Owner.City);
+console.log(car)

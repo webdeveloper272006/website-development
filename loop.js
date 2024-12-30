e1dbeaefe835d4b44b2fd7268262db59af3ff64e
webdeveloper272006 <web213783@gmail.com>
2024-12-30 14:42:26 +0500

    Add files via upload

--- loop.js
+++ loop.js
@@ -25,33 +25,57 @@
     
 // Question:1
 
-for (let index = 0; index <3; index++) {
-    console.log("Hello");
-    ;    
-}
+// for (let index = 0; index <3; index++) {
+//     console.log("Hello");
+//     ;    
+// }
 
-// Question:2
+// // Question:2
 
-let books = ['chem','bio','eng','urdu','phy']
+// let books = ['chem','bio','eng','urdu','phy']
 
-for (let index = 0; index < books.length; index++) {
-    const element = books[index];
-    console.log(element);
+// for (let index = 0; index < books.length; index++) {
+//     const element = books[index];
+//     console.log(element);
+    
+// }
+
+// // Question :3
+
+// const employ ={
+//     name : "Haris",
+//     age : 25,
+//     email : "abc.com",
+//      ph_No : 82776465,
+// }
+
+// for (const key in employ) {
+//     const element = employ[key];
+//     console.log(`The ${key} is ${element}`);
+    
+// }
+
+// let i = 0
+// while (i<8){
+//     console.log(`count: ${i}`);
+//     i++
+// }
+
+// QUESTION :1 Write a code that prints sum of 10 numbers
+
+let sums = 0
+for (let nums = 0; nums <=10 ; nums++) {
+    sums+=nums
     
 }
+console.log(sums);
 
-// Question :3
+// QUESTON :2 Write a table of 4
 
-const employ ={
-    name : "Haris",
-    age : 25,
-    email : "abc.com",
-     ph_No : 82776465,
+let table = 4
+for (let value = 1; value <=10 ; value++){
+   work= table*value
+    console.log(`4*${value} = ${work}`);
+
 }
-
-for (const key in employ) {
-    const element = employ[key];
-    console.log(`The ${key} is ${element}`);
-    
-}
-
+   

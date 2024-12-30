f89181b42f6c17a97f42e651d404d208ea5b220e
webdeveloper272006 <web213783@gmail.com>
2024-12-30 13:05:43 +0500

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,57 @@
+// for (let index = 0; index < 5; index++) {
+// console.log(index);
+// ;
+    
+// }
+
+// let bakery= ['apple','milk','eggs','cakes']
+
+// for (let index = 0; index < bakery.length; index++) {
+//     const element = bakery[index];
+//     console.log(element);
+// }
+
+// let person={
+//     Name : "john Sen",
+//     Age : 24,
+//     isStudent : true,
+// }
+
+// for (const key in person){
+//     const element = person[key];
+//     console.log(`the ${key} is ${element}`);
+    
+// }
+    
+// Question:1
+
+for (let index = 0; index <3; index++) {
+    console.log("Hello");
+    ;    
+}
+
+// Question:2
+
+let books = ['chem','bio','eng','urdu','phy']
+
+for (let index = 0; index < books.length; index++) {
+    const element = books[index];
+    console.log(element);
+    
+}
+
+// Question :3
+
+const employ ={
+    name : "Haris",
+    age : 25,
+    email : "abc.com",
+     ph_No : 82776465,
+}
+
+for (const key in employ) {
+    const element = employ[key];
+    console.log(`The ${key} is ${element}`);
+    
+}
+
